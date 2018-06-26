850d30aedd7bdbc218cfee6d68df3a48aed87d00
Nate Dukatz <natedukatz@gmail.com>
2018-06-26 04:20:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,8 +17,8 @@ var destructivelyRemoveElementFromBeginningOfArray = array => {
   return array
 }
 
-// var removeElementFromBeginningOfArray = array => array.slice(1)
-//
+var removeElementFromBeginningOfArray = array => array.slice(1)
+
 // var destructivelyRemoveElementFromEndOfArray = array => array.pop() return array
 //
 // var removeElementFromEndOfArray = array => array.slice(0, array.length - 1)
