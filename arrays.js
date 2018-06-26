108b007f51e3cd02ad9935f73c5faf1d5c40637e
Nate Dukatz <natedukatz@gmail.com>
2018-06-26 03:37:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-var addElementToBeginningOfArray = (array, element) => var newArray = [element, ...array]
-
-var destructivelyAddElementToBeginningOfArray = (array, element) => array.unshift(element) return array
-
-var accessElementInArray = (array, index) => array[index]
-
-var destructivelyRemoveElementFromBeginningOfArray = array => array.shift() return array
-
-var removeElementFromBeginningOfArray = array => array.slice(1)
-
-var destructivelyRemoveElementFromEndOfArray = array => array.pop() return array
-
-var removeElementFromEndOfArray = array => array.slice(0, array.length - 1)
